4af92f67035138d03b21b9f5b5901745393e4376
fialo4ka <tona16@inbox.lv>
2021-04-06 09:51:34 +0200

    add oauth login in frontend

--- reactbill/src/index.js
+++ reactbill/src/index.js
@@ -4,6 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+var ClientOAuth2 = require('client-oauth2')
+
+function getNonce(length) {
+  const array = new Uint8Array(length);
+  window.crypto.getRandomValues(array);
+  return btoa(String.fromCharCode.apply(null, array));
+}
+
+var oauthAuth = new ClientOAuth2({
+  clientId: 'reactbill',
+  clientSecret: 'paJee8chaequohFai5Poh8ioB6eiboh9',
+  accessTokenUri: 'https://oauth2.o-g.at/oauth2/token',
+  authorizationUri: 'https://oauth2.o-g.at/oauth2/auth',
+  redirectUri: 'http://localhost:3000/auth/oauth2/callback',
+  scopes: ['openid', 'microbill'],
+  state: getNonce(10)
+})
+
+window.oauth2Callback = function (uri) {
+  oauthAuth.token.getToken(uri)
+    .then(function (user) {
+      console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... }
+
+    })
+}
+
+// Open the page in a new window, then redirect back to a page that calls our global `oauth2Callback` function.
+window.open(oauthAuth.token.getUri())
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
